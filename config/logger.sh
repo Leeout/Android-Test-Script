1cc144f7702c60541f9ed015c071a3ad80194370
李杰04 <lijie04@qutoutiao.net>
2019-12-20 14:37:14 +0800

    update

--- config/logger.sh
+++ config/logger.sh
@@ -1,5 +1,5 @@
 logger_debug(){
-  echo "$(date "+%Y-%m-%d %H:%M:%S") -- PID:$$  ""\033[32m $1 \033[0m"
+  echo "\033[32m $(date "+%Y-%m-%d %H:%M:%S") -- PID:$$   $1 \033[0m"
 }
 
 logger_debug test
